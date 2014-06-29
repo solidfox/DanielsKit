f410894d6cdc15a3a1286bff341beb60b9582543
Daniel Schlaug <daniel.schlaug@gmail.com>
2014-06-29 23:12:16 +0200

    Initial Commit

---
+++ DanielsKit/DanielsKit.h
@@ -0,0 +1,19 @@
+//
+//  DanielsKit.h
+//  DanielsKit
+//
+//  Created by Daniel Schlaug on 6/29/14.
+//  Copyright (c) 2014 Daniel Schlaug. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DanielsKit.
+FOUNDATION_EXPORT double DanielsKitVersionNumber;
+
+//! Project version string for DanielsKit.
+FOUNDATION_EXPORT const unsigned char DanielsKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DanielsKit/PublicHeader.h>
+
+
